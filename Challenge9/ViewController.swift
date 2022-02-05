293656c22f13d308b316a448a688beaa9d8fa3ab
Keith Crooc <keith.crooc@gmail.com>
2022-02-05 17:09:41 -0500

    added functionality of both add text buttons. Uses elements of project29 to draw the text ontop of an image.

--- Challenge9/ViewController.swift
+++ Challenge9/ViewController.swift
@@ -24,19 +24,62 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet var imageView: UIImageView!
     
+    @IBOutlet var topTextDisplay: UIImageView!
+    @IBOutlet var bottomTextDisplay: UIImageView!
+    
+    var topText: String = ""
+    var bottomText: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         title = "Meme Generator"
+        
+        displayTopText(topCaption: topText)
     }
     
     @IBAction func writeTopText(_ sender: Any) {
+        let ac = UIAlertController(title: "Enter Top Text", message: nil, preferredStyle: .alert)
+        ac.addTextField()
         
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [unowned ac] _ in
+            let message = ac.textFields![0]
+            
+            self.topText = message.text!
+            
+            
+            self.displayTopText(topCaption: message.text!)
+            
+            
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
         
     }
     
 
     @IBAction func writeBottomText(_ sender: Any) {
+        let ac = UIAlertController(title: "Enter bottom text", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [unowned ac] _ in
+            let message = ac.textFields![0]
+            
+            self.bottomText = message.text!
+            
+            self.displayBottomText(bottomCaption: message.text!)
+            
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
     }
     
     @IBAction func uploadPhoto(_ sender: Any) {
@@ -54,6 +97,66 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         imageView.image = image
         
+        
+    }
+    
+    func displayTopText(topCaption: String) {
+        
+        print(topCaption)
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 375, height: 100))
+        
+        let msg = renderer.image {
+            ctx in
+            
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            
+            let attrs: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 25),
+                .paragraphStyle: paragraphStyle,
+                .strokeWidth: 8.5,
+                .strokeColor: UIColor.white.cgColor,
+                .backgroundColor: UIColor.black.cgColor
+                
+            ]
+            
+            let string = topCaption
+            let attributedString = NSAttributedString(string: string, attributes: attrs)
+            
+            attributedString.draw(with: CGRect(x: 0, y: 0, width: 370, height: 100), options: .usesLineFragmentOrigin, context: nil)
+        }
+        
+        topTextDisplay.image = msg
+        
+    }
+    
+    func displayBottomText(bottomCaption: String) {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 375, height: 100))
+        
+        let msg = renderer.image {
+            ctx in
+            
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            
+            let attrs: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 25),
+                .paragraphStyle: paragraphStyle,
+                .strokeWidth: 8.5,
+                .strokeColor: UIColor.white.cgColor,
+                .backgroundColor: UIColor.black.cgColor
+                
+            ]
+            
+            let string = bottomCaption
+            let attributedString = NSAttributedString(string: string, attributes: attrs)
+            
+            attributedString.draw(with: CGRect(x: 0, y: 0, width: 370, height: 100), options: .usesLineFragmentOrigin, context: nil)
+        }
+        
+        bottomTextDisplay.image = msg
         
     }
     
